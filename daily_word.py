2f8bdb44aa49a6cc8a6cd02c7eca0935619b3543
fstempfel <francois-xavier.stempfel@atos.net>
2019-05-27 15:43:32 +0200

    Still need to understand use of tokens and authentication

--- daily_word.py
+++ daily_word.py
@@ -1,8 +1,13 @@
 import base64
-import json
+import os
+import pickle
 
 from email.mime.text import MIMEText
 from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
 
 
 class EMail:
@@ -27,14 +32,30 @@ class EMail:
         message['to'] = to
         message['from'] = sender
         message['subject'] = subject
-        return base64.urlsafe_b64encode(message.as_string())
+        return base64.urlsafe_b64encode(message.as_bytes())
 
     @staticmethod
     def build_service():
-        with open("credentials.json", "r") as f:
-            credentials = json.load(f)
+        creds = None
+        # The file token.pickle stores the user's access and refresh tokens, and is
+        # created automatically when the authorization flow completes for the first
+        # time.
+        if os.path.exists('token.pickle'):
+            with open('token.pickle', 'rb') as token:
+                creds = pickle.load(token)
+        # If there are no (valid) credentials available, let the user log in.
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    'credentials.json', SCOPES)
+                creds = flow.run_local_server()
+            # Save the credentials for the next run
+            with open('token.pickle', 'wb') as token:
+                pickle.dump(creds, token)
 
-        return build("gmail", "v1", credentials)
+        return build("gmail", "v1", creds)
 
     def send_message(self, user_id="me"):
         """Send an email message.
