882ddf81b0c59182ca6ec0189cc68af605d03ba8
Fxstempfel <fxstempfel@gmail.com>
2019-05-28 22:56:07 +0200

    Works with remote dict/still needs robustness

--- daily_word.py
+++ daily_word.py
@@ -1,28 +1,29 @@
 from datetime import datetime
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from lxml import etree
 
 import locale
 import random
 import requests
 import smtplib
+import string
 
 import login
 
 DATE_FORMAT_RECORD = "%Y%m%d"
 DATE_FORMAT_MSG = "%A %d %B"
 
-SIZE_DICT = 22740
+URL_BASE = "https://www.le-dictionnaire.com"
 
-URL_DICT = "https://www.le-dictionnaire.com/definition/{word}"
-
-PATH_DICT = "liste_francais.txt"
 PATH_RECORD = "record.txt"
 
 SUBJECT = "Le Mot du Jour"
 TOs = [
     "fxstempfel@gmail.com"
 ]
+
+URL_LETTER = "https://www.le-dictionnaire.com/repertoire/{letter}01.html"
 
 BODY = """
 <html>
@@ -62,27 +63,23 @@ class EMail:
 class WordPicker:
     @staticmethod
     def pick():
-        while True:
-            nb_line = random.randint(0, SIZE_DICT)
-            print(nb_line)
-            with open(PATH_DICT, "r") as f:
-                for i, line in enumerate(f):
-                    word = line.strip("\n")
-                    if i == nb_line and WordPicker.check_word_ok(word):
-                        return word
+        letter = random.choice(string.ascii_letters)
+        print("LETTRE", letter)
+        response = requests.get(URL_LETTER.format(letter=letter))
+        root = etree.HTML(response.content)
+        print(etree.tostring(root, pretty_print=True, encoding="unicode"))
+        selected_range = random.choice(root.xpath("//span[div[@class='titregroupelettre']]/a")).attrib["href"]
 
-    @staticmethod
-    def check_word_ok(word):
-        r = requests.get(URL_DICT.format(word=word))
-        if r.status_code != 200:
-            return False
-
-        return "<h3>Définition de {}</h3>".format(word) in r.content.decode("utf8")
+        response = requests.get(URL_BASE + selected_range)
+        root = etree.HTML(response.content)
+        list_words_a = root.xpath("//div[@class='alphabox']/ul/li/a")
+        word_a = random.choice(list_words_a)
+        return word_a.text, URL_BASE + word_a.attrib["href"]
 
 
 class Master:
     @staticmethod
-    def update_record(word):
+    def update_record(word, url_word):
         try:
             with open(PATH_RECORD, "r") as f:
                 lines = f.readlines()
@@ -92,7 +89,7 @@ class Master:
             lines = []
 
         date = datetime.now().strftime(DATE_FORMAT_RECORD)
-        new_line = "{}:{}\n".format(date, word)
+        new_line = "{}##{}##{}\n".format(date, word, url_word)
         with open(PATH_RECORD, "w") as f:
             f.write(new_line)
             for line in lines[:min(nb_words, 7)]:
@@ -108,20 +105,20 @@ class Master:
 
         res = {}
         for line in lines:
-            date, word = line.split(":")
-            res[datetime.strptime(date, DATE_FORMAT_RECORD).strftime(DATE_FORMAT_MSG)] = word.strip("\n")
+            date, word, url = line.split("##")
+            res[datetime.strptime(date, DATE_FORMAT_RECORD).strftime(DATE_FORMAT_MSG)] = word, url.strip("\n")
         return res
 
     @staticmethod
-    def format_email(word, to):
+    def format_email(word, url, to):
         dict_old = Master.read_record()
         if len(dict_old) == 0:
             history = ""
         else:
             try:
                 history = "Petit rappel des derniers mots :" \
-                          + "\n".join(['<li>{} : <a href="{}">{}</a></li>'.format(date, URL_DICT.format(word=old_word), old_word)
-                                       for date, old_word in dict_old.items()]) \
+                          + "\n".join(['<li>{} : <a href="{}">{}</a></li>'.format(date, old_url, old_word)
+                                       for date, (old_word, old_url) in dict_old.items()]) \
                           + "\n"
             except AttributeError:
                 history = ""
@@ -132,22 +129,22 @@ class Master:
         body = BODY.format(to=to,
                            subject=SUBJECT,
                            today=word,
-                           url=URL_DICT.format(word=word),
+                           url=url,
                            history=history)
 
         return body
 
     @staticmethod
     def main():
-        new_word = WordPicker.pick()
+        new_word, url = WordPicker.pick()
 
         locale.setlocale(locale.LC_ALL, 'fr_FR.utf8')
 
         for to in TOs:
-            message_text = Master.format_email(new_word, to)
+            message_text = Master.format_email(new_word, url, to)
             EMail("Daily Word", to, SUBJECT, message_text).send()
 
-        Master.update_record(new_word)
+        Master.update_record(new_word, url)
 
 
 if __name__ == '__main__':
